4a81fdbc9a84df26a2f835fcfb75849862bd1bb3
Priyankateke <priiyankateke1705@gmail.com>
2020-05-17 09:48:09 +0530

    ADDED: Assigning Symbol 'X' Or 'O' For Player

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,8 @@
 console.log("welcome To Tic Tac Toe")
 
+//variables
+let player;
+
 //declaring array
 let gameBoard = [];
 
@@ -8,4 +11,17 @@ function resetBoard() {
  gameBoard = [1,2,3,4,5,6,7,8,9];
 }
 
+/**
+ * Assigning Sign To Player
+ */
+function assignSignToPlayer() {
+    let randomValue = Math.floor(Math.random() * 2)
+    if(randomValue == 1) {
+        player = 'X'
+    }
+    else {
+        player = 'O'
+    }
+}
 resetBoard()
+assignSignToPlayer()
