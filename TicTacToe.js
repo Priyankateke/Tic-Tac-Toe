4d9c50bf1f93acf9b2d6f544df4e60c1b6206c36
Priyankateke <priiyankateke1705@gmail.com>
2020-05-17 09:34:36 +0530

    ADDED: Reset Game Board

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,1 +1,11 @@
-console.log("Welcome To Tic Tac Toe")
+console.log("welcome To Tic Tac Toe")
+
+//declaring array
+let gameBoard = [];
+
+//resetting board
+function resetBoard() {
+ gameBoard = [1,2,3,4,5,6,7,8,9];
+}
+
+resetBoard()
