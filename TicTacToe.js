31f234fb4f5c6c78cb61508e20edebafe4a6239a
Priyankateke <priiyankateke1705@gmail.com>
2020-05-21 17:07:50 +0530

    ADDED: Check If Computer Can Win Then Place On Win Position

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,170 +1,199 @@
-console.log("welcome To Tic Tac Toe")
+//For accepting user input
 var read = require('readline-sync')
-const Process =require('process')
 
-//constants
-let TOTAL_MOVES = 9;
+//process
+const Process = require('process')
 
 //variables
-let player;
-let computer;
-let playerMoves = 0;
-let currentPlayer = 0;
-let flag
-//let position
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let choice;
+flag = 0;
+let moveCompleted = 0;
+let totalMove = 9;
 
-//declaring array
-let gameBoard = [];
+class TicTacToe {
+    /**
+     * Start Game
+     */
+    playTillGameEnd() {
 
-//resetting board
-function resetBoard() {
-    gameBoard = [1,2,3,4,5,6,7,8,9];
-    displayBoard()
-}
-
-/**
- * displaying Board
- */
-function displayBoard() {
-    console.log("-------------")
-    for(let i = 0; i < 9; i += 3) {
-        console.log("| "+ gameBoard[i] + " | "+ gameBoard[(i+1)] + " | "+ gameBoard[(i+2)] +" | ")
-        console.log("-------------")
-    }
-}
-
-/**
- * Assiging letter X or O to player and decide who play first
- */
-function tossForPlay() {
-    let randomValue = Math.floor(Math.random() * 2)
-    if(randomValue == 1) {
-        computer = 'X'
-        player = 'O'
-    }
-    else {
-        player = 'X'
-        computer = 'O'
-    }
-    if( player == 'X' ) {
-        console.log("Player play first with X sign ")
-        playerTurn()
-    }
-    else {
-        console.log("Computer play first with X sign")
-        computerTurn()
-    }
-}
-
-/**
- * Switch Player Sign
- */
-function switchPlayer() {
-   if( currentPlayer == 1 ) {
-       computerTurn(0)
-   } 
-   else {
-       playerTurn(0)
-   }
-}
-
-/**
- * Function For User Play
- */
-function playerTurn(flag) {
-    if(flag == 0 ) {
-        console.log("Player Turn Sign : "+player)
-    }
-    let position = read.question("Enter Position Between 1 to 9 : " )
-    if( position >= 1 && position <= 9 ) {
-        isCellEmpty(position, player)
-        currentPlayer=1
-    }
-    else {
-        console.log("Invalid Position")
-        playerTurn()
-    }
-}
-
-function computerTurn(flag) {
-    if(flag == 0 ) {
-        console.log("computer Turn Sign " + computer)
-    }
-    let randomPosition = Math.floor(Math.random() * 9 + 1)
-    isCellEmpty(randomPosition, computer)
-    currentPlayer = 0
-}
-
-/**
- * 
- * @param {*} position 
- * @param {*} sign 
- */
-function isCellEmpty(position, sign, flag) {
-    let index = position-1
-    if( gameBoard[index] != 'X' && gameBoard[index] != 'O' ) {
-        gameBoard[index] = sign
-        playerMoves++
-    }
-    else {
-        if(currentPlayer == 0) {
-            console.log("Position is occupied : ")
-            playerTurn(1)
-        } 
-        else {
-            computerTurn(1)
-        }
-    }
-}
-
-
-/**
- * Checking column, rows and diagonals
- */
-function checkWinningCells() {
-    let col = 0
-    for( let row = 0; row < 7; row += 3 ) {
-        checkWinner(gameBoard[row], gameBoard[(row+1)], gameBoard[(row+2)])
-        checkWinner(gameBoard[col], gameBoard[(col+3)], gameBoard[(col+6)])
-        col++
-    }
-    checkWinner(gameBoard[0], gameBoard[4], gameBoard[8])
-    checkWinner(gameBoard[2], gameBoard[4], gameBoard[6])
-}
-
-/**
- * Checking Winner
- * @param {*} cell1 
- * @param {*} cell2 
- * @param {*} cell3 
- */
-function checkWinner(cell1, cell2, cell3) {
-    let winner;
-    if( cell1 == cell2 && cell2 == cell3 ) {
-        if( cell1 == player ) {
-            winner = "Player"
+        console.log("Player:X and Computer:O\n");
+        let isCompTurn = false;
+        let random = Math.floor(Math.random() * 2)
+        if (random == 0) {
+            isCompTurn = true;
+            console.log("Computer Play first \n");
         }
         else {
-            winner = "Computer"
+            console.log("Player Play first \n");
         }
-        console.log(winner+" Win and have sign " + cell1)
-        Process.exit()
+
+        this.displayBoard();
+        
+        do {
+            if (isCompTurn) {
+                isCompTurn = true;
+                console.log("Computer chance");
+            }
+            else {
+                isCompTurn = false;
+                console.log("Player chance");
+                choice = read.question("Enter Position Between 1 to 9 : ");             
+            }
+            if (isCompTurn || (arr[choice] != 'X' && arr[choice] != 'O')) {
+                if (isCompTurn) {
+                    this.computerMove('X', 'O');
+                    moveCompleted++;
+                    this.displayBoard();
+                    isCompTurn = false;
+                }
+                else {
+                    arr[choice] = 'X';
+                    moveCompleted++;
+                    this.displayBoard();
+                    isCompTurn = true;
+                }
+            }
+            else {
+                console.log("The position is already marked with " + arr[choice]);
+                console.log("\n");
+                console.log("Enter again");
+            }
+            flag = this.checkWin();
+
+        } while (flag != 1 && flag != -1 && moveCompleted != totalMove)
+
+        this.displayBoard();
+
+        if (flag == 1) {
+            let winner = isCompTurn ? "Player" : "Computer";
+            console.log(winner + " has won");
+        }
+        else {
+            console.log("Draw");
+        }
+    }
+
+    /**
+     * check player winning condition
+     */
+    checkWin() {
+        //Check row wise win
+        for (let i = 1; i <= 9;) {
+            if (arr[i] == arr[i + 1] && arr[i + 1] == arr[i + 2]) {
+                return 1;
+                break;
+            }
+            i = i + 3;
+        }
+        //Check column wise win
+        for (let i = 1; i <= 3; i++) {
+            if (arr[i] == arr[i + 3] && arr[i + 3] == arr[i + 6]) {
+                return 1;
+                break;
+            }
+        }
+        //Check diagonal win
+        if (arr[1] == arr[5] && arr[5] == arr[9]) {
+            return 1;
+        }
+        else if (arr[3] == arr[5] && arr[5] == arr[7]) {
+            return 1;
+        }
+        return 0; //play continue
+    }
+    /**
+     * computer move turn
+     * @param {*} playerChar 
+     * @param {*} compChar 
+     */
+    computerMove(playerChar, compChar) {
+
+        //Start Check Win Cells
+        if (this.checkWinAndBlock(1, 2, 3, compChar)) { }
+        else if (this.checkWinAndBlock(4, 5, 6, compChar, compChar)) { }
+        else if (this.checkWinAndBlock(7, 8, 9, compChar, compChar)) { }
+        else if (this.checkWinAndBlock(1, 4, 7, compChar, compChar)) { }
+        else if (this.checkWinAndBlock(2, 5, 8, compChar, compChar)) { }
+        else if (this.checkWinAndBlock(3, 6, 9, compChar, compChar)) { }
+        else if (this.checkWinAndBlock(1, 5, 9, compChar, compChar)) { }
+        else if (this.checkWinAndBlock(3, 5, 7, compChar, compChar)) { }
+        //End Check Win Cells
+
+        //start diagonal and side cells
+        else {
+            this.checkDiagonalCenterSideCell(compChar);
+        }
+        //end diagonal and side cells
+    }
+
+   /**
+    * computer will check for win and block opponent
+    * @param {*} cell1 
+    * @param {*} cell2 
+    * @param {*} cell3 
+    * @param {*} checkChar 
+    * @param {*} compChar 
+    */
+    checkWinAndBlock(cell1, cell2, cell3, checkChar, compChar) {
+        let allocated = false;
+        for (let i = 0; i < 3; i++) {
+            if (arr[cell1] == checkChar && arr[cell2] == checkChar && this.isEmpty(arr[cell3])) {
+                arr[cell3] = compChar;
+                allocated = true;
+                break;
+            }
+            else {
+                let temp = cell1;
+                cell1 = cell2;
+                cell2 = cell3;
+                cell3 = temp;
+            }
+        }
+        return allocated;
+    }
+
+    /**
+     * check Valid Cell
+     * @param {*} val 
+     */
+    isEmpty(val) {
+        let isBlank = false;
+        if (val != 'X' && val != 'O') {
+            isBlank = true;
+        }
+        return isBlank;
+    }
+
+    /**
+     * check corners, sides, center are available or not
+     * @param {*} compChar 
+     */
+    checkDiagonalCenterSideCell(compChar) {
+        let allocated = false;
+        let cells = [1, 3, 7, 9, 5, 2, 4, 6, 8];
+        for (let i = 0; i < cells.length; i++) {
+            if (this.isEmpty(arr[cells[i]])) {
+                arr[cells[i]] = compChar;
+                allocated = true;
+                break;
+            }
+        }
+        return allocated;
+    }
+
+    /**
+     * display board
+     */
+    displayBoard() {
+        console.log("__________");
+        console.log(arr[1] + " | " + arr[2] + " | " + arr[3]);
+        console.log("__|___|___");
+        console.log(arr[4] + " | " + arr[5] + " | " + arr[6]);
+        console.log("__|___|___");
+        console.log(arr[7] + " | " + arr[8] + " | " + arr[9]);
+        console.log("__________");
+
     }
 }
-
-/**
- * Running game untill game ends
- */
-function playTillGameEnd() {
-    resetBoard()
-    tossForPlay()
-    while( playerMoves < TOTAL_MOVES ) {
-        displayBoard()
-        checkWinningCells()
-        switchPlayer()
-    }
-    displayBoard()
-    console.log("Game Tie")
-}
-
-playTillGameEnd()
+module.exports = TicTacToe
---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("welcome To Tic Tac Toe")
+
+const TicTacToe = require('./TicTacToe')
+
+const ticTacToe = new TicTacToe()
+ticTacToe.playTillGameEnd()
